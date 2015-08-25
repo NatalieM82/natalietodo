32acbb13aa902138f8fa985bda404418dfc8272a
Natalie Menahem <NatalieMenahem@Natalies-MacBook-Pro.local>
2015-08-26 01:21:11 +0300

    First commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+
+module.exports = {
+    url : 'mongodb://natalie:tuti1007@ds035533.mongolab.com:35533/mytodo'
+};
---
+++ public/js/services/todos.js
@@ -0,0 +1,24 @@
+// js/services/todos.js
+angular.module('todoService', [])
+
+    // super simple service
+    // each function returns a promise object 
+    .factory('Todos', function($http) {
+        return {
+            get : function() {
+                return $http.get('/api/todos');
+            },
+            create : function(todoData) {
+                return $http.post('/api/todos', todoData);
+            },
+            delete : function(id) {
+                return $http.delete('/api/todos/' + id);
+            },
+            done : function(id) {
+                 return $http.post('/api/todos/done/' + id);
+            },
+            edit : function(id, text) {
+                 return $http.post('/api/todos/edit/' + id +'/' + text);
+            }
+        }
+    });
